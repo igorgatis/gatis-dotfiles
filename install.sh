f314ee2600f733d69beef21d47487d410ddaf9d0
Igor Gatis <igorgatis@gmail.com>
2024-02-23 12:13:19 -0300

    add logic to check for macos

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 set -x
 cd ~/
-ln -sf dotfiles/.bash_profile
+
 ln -sf dotfiles/.bashrc
 ln -sf dotfiles/.gitconfig
 ln -sf dotfiles/.tmux.conf
 ln -sf dotfiles/.vimrc
 ln -sf dotfiles/bin
+
+osname=$(uname)
+if [ "$osname" == "Darwin" ]; then
+  chsh -s /bin/bash
+  ln -sf dotfiles/.bash_profile
+fi
+
