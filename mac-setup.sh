74857a7a6f0306b24db64aac07846e1fe630e61b
Igor Gatis <igorgatis@gmail.com>
2024-07-09 11:05:30 -0300

    Added mac setup and docker-run command

---
+++ mac-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -x
+
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+
+brew install --cask rectangle
+brew install --cask alt-tab
+brew install --cask iterm2
+
