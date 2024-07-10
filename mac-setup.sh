e9768c418e6b6f6fac8b5b45a684bc6778c20888
Igor Gatis <igorgatis@gmail.com>
2024-07-10 14:42:33 -0300

    Update mac-setup.sh

--- mac-setup.sh
+++ mac-setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
-set -e
 set -x
 
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
+set -e
 brew install --cask rectangle
 brew install --cask alt-tab
 brew install --cask iterm2
