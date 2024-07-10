3d811d845a13db666014da8950b009c3f48c267a
Igor Gatis <igorgatis@gmail.com>
2024-07-10 14:31:38 -0300

    Update mac-setup.sh

--- mac-setup.sh
+++ mac-setup.sh
@@ -8,4 +8,4 @@ curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 brew install --cask rectangle
 brew install --cask alt-tab
 brew install --cask iterm2
-
+brew install --cask visual-studio-code
