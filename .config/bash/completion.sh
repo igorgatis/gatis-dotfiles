30ececb022fc25d7a8c5ca402210078b7e427df8
Igor Gatis <igorgatis@gmail.com>
2025-01-09 12:52:13 -0300

    Added completions

--- .config/bash/completion.sh
+++ .config/bash/completion.sh
@@ -14,10 +14,24 @@ source "$HOME/.config/bash/git-completion.bash"
 _git_dd() { _git_diff ; }
 _git_p() { _git_diff ; }
 
-source "$HOME/.config/bash/kubectl-completion.sh"
+if command -v docker &> /dev/null; then
+    source <(docker completion bash)
+fi
 
-alias k=kubectl
-complete -o default -F __start_kubectl k
-# Namespace related:
-alias kx='f() { [ "$1" ] && kubectl config use-context $1 || kubectl config get-contexts ; } ; f'
-alias kn='f() { [ "$1" ] && kubectl config set-context --current --namespace $1 || kubectl config view --minify | grep namespace | cut -d" " -f6 ; } ; f'
+if command -v kubectl &> /dev/null; then
+    source "$HOME/.config/bash/kubectl-completion.sh"
+
+    alias k=kubectl
+    complete -o default -F __start_kubectl k
+    # Namespace related:
+    alias kx='f() { [ "$1" ] && kubectl config use-context $1 || kubectl config get-contexts ; } ; f'
+    alias kn='f() { [ "$1" ] && kubectl config set-context --current --namespace $1 || kubectl config view --minify | grep namespace | cut -d" " -f6 ; } ; f'
+fi
+
+if command -v minikube &> /dev/null; then
+    source <(minikube completion bash)
+fi
+
+if command -v helm &> /dev/null; then
+    source <(helm completion bash)
+fi
