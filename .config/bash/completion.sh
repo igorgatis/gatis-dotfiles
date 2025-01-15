126f28e69d30d2d2f83dec9313990c2a3d70a444
Igor Gatis <igorgatis@gmail.com>
2025-01-15 14:44:14 -0300

    fixed kubenertes commands

--- .config/bash/completion.sh
+++ .config/bash/completion.sh
@@ -20,7 +20,7 @@ if command -v kubectl &> /dev/null; then
     alias k=kubectl
     complete -o default -F __start_kubectl k
     # Namespace related:
-    alias kx='f() { [ "$1" ] && kubectl config use-context $1 || kubectl config get-contexts ; } ; f'
-    alias kn='f() { [ "$1" ] && kubectl config set-context --current --namespace $1 || kubectl config view --minify | grep namespace | cut -d" " -f6 ; } ; f'
+    alias kx='f() { [ "$1" ] && kubectl config use-context "$1" || kubectl config get-contexts ; } ; f'
+    alias kn='f() { [ "$1" ] && kubectl config set-context --current --namespace "$1" || kubectl config get-contexts ; } ; f'
 fi
 
